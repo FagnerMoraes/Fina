9b56ba281c86c19469db7a71a6d277323b824b89
FagnerMoraes <fagm.carvalho@gmail.com>
2025-03-19 17:19:29 -0300

    Adicionando Handlers e Response

---
+++ Fina.Core/Handles/ICategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿using Fina.Core.Models;
+using Fina.Core.Requests.Categories;
+using Fina.Core.Responses;
+
+namespace Fina.Core.Handles;
+
+public interface ICategoryHandler
+{
+    Task<Response<Category?>> CreateAsync(CreateCategoryRequest request);
+    Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request);
+    Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request);
+    Task<Response<Category?>> GetByIdAsync(GetCategorybyIdRequest request);
+    Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoriesRequest request);
+}

ea6fb0f2f95b492754fff5dcb0b193edef9f565b
FagnerMoraes <fagm.carvalho@gmail.com>
2025-03-21 09:39:26 -0300

    Mapeando objetos para  o banco e criando primeira migracao

--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,4 +1,15 @@
+using Fina.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SecretConnection"));
+});
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
